9e49de7c7cdead6b535f555a4d5d186c944bf514
jjchavarrg <jjchavarrg@eafit.edu.co>
2023-10-25 21:41:16 -0500

    feat: update config db & test data

--- config/db.js
+++ config/db.js
@@ -1,5 +1,18 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect();
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(
+      `MongoDB Connected: ${mongoose.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error}`.red.underline.bold);
+  }
 };
+
+module.exports = connectDB;
